b68d9bfefbaf8137705fb66f65704c1a347ff468
Ufuk Hamit Alkin <49322056+SinhtUp@users.noreply.github.com>
2022-07-11 15:04:05 +0200

    deploy my react app to git

---
+++ react-deploy/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "App" >
+        <div> Hello World!ufuk </div> 
+        <image className= "App-logo" src={logo}/>
+		</div>
+    );
+}
+
+export default App;
